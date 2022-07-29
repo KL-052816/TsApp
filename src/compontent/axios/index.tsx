3632a681afe9765df7e4793343b3fca93567e4b2
banshi <1303771879@qq.com>
2022-07-29 17:51:56 +0800

    fix(koc):第一次+++
    fix(koc):第二次+++
    
    fix(koc):第三次+++
    
    fix(koc):第四次+++

--- src/compontent/axios/index.tsx
+++ src/compontent/axios/index.tsx
@@ -10,7 +10,7 @@ function Axios(){
     const [data,setData] = useState<dataTask>();
     console.log('commit-diyici')
     console.log('qcommit-dierci')
-
+console.log('2111111111')
     function getList(){
         axios.request({
             url:`${api}/api/v1/topics`,
